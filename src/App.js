0ea5b317eff9c5910dcc40afffff4d9cf55073ee
luozijing <908364810@qq.com>
2019-06-14 17:44:59 +0800

    Save before ejecting

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    target="_blank" href="https://reactjs.org"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
         </a>
       </header>
     </div>
